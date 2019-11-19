6e4c2c82bc896fd9821390280c4313588134cd34
Dirane <diranengala@gmail.com>
2019-11-19 07:08:25 +0100

    Edited the docstring of the time stats

--- bikeshare.py
+++ bikeshare.py
@@ -103,7 +103,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel(Month, week, day and start hour)."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
